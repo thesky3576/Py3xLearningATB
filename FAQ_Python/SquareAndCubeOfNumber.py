210f10445d172b04ad4c71bc2a2db99b349216d2
Akash Bhosale <bhosaleav.official@gmail.com>
2024-10-19 10:19:10 +0530

    this is latest commit

---
+++ FAQ_Python/SquareAndCubeOfNumber.py
@@ -0,0 +1,6 @@
+a = int(input("Enter value for a for square and cube: "))
+def Square(a):
+    return a ** 2
+def cube(a):
+    return a ** 3
+print(f"The Square of given number is {Square(a)} and cube for given number is {cube(a)}")
